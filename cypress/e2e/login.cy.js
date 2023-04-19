c0296d569465ef1e833d177e84603558e8e5aae9
Oscar Alcivar <eloskr16@gmail.com>
2023-04-19 17:17:49 +0200

    correct test on cypress

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,11 +9,13 @@ describe('Test login', () => {
     cy.get('[data-cy="mail"]').type('admin@admin.com')
     cy.get('[data-cy="password"]').type('admin')
     cy.get('[data-cy="submit"]').click()
+    cy.url().should('eq','http://localhost:3000/miranda_dashboard')
   })
 
   it("Check login not succesful", () => {
     cy.get('[data-cy="mail"]').type('a')
     cy.get('[data-cy="password"]').type('a')
     cy.get('[data-cy="submit"]').click()
+    cy.url().should('eq','http://localhost:3000/miranda_dashboard/login')
   })
 })
